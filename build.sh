7be999ec50047174e1a50f529567ac7722397137
Matthew Blissett <mblissett@gbif.org>
2019-04-15 16:15:11 +0200

    Talbe to asciidoc, create translation index.

--- build.sh
+++ build.sh
@@ -1,19 +1,15 @@
 #!/bin/bash -e
 
-# Run in:
-#   docker run --rm -it -v $PWD:/documents/ asciidoctor/docker-asciidoctor:1.5.7.1
-
 # Enable the **/*.en.adoc below.
 shopt -s globstar
-# In case there are no translations.
-shopt -s nullglob
 
 # Produce the translated adoc source from the po-files.
+po4a -v po4a.conf
 for lang in translations/??.po; do
 	langcode=$(basename $lang .po)
-	for doc in **/*.en.adoc; do
-		po4a-translate -f asciidoc -M utf-8 -m $doc -p $lang -k 0 -l $(dirname $doc)/$(basename $doc .en.adoc).$langcode.adoc
-	done
+	#for doc in **/*.en.adoc; do
+	#	po4a-translate -f asciidoc -M utf-8 -m $doc -p $lang -k 0 -l $(dirname $doc)/$(basename $doc .en.adoc).$langcode.adoc
+	#done
 	# Convert some includes to refer to the translated versions (this needs improvement).
 	perl -pi -e 's/([A-Za-z0-9_-]+).en.adoc/\1.'$langcode'.adoc/' index.$langcode.adoc
 done
@@ -23,18 +19,18 @@ rm -f **/*.??.html **/*.??.pdf *.??.asis
 for lang in en translations/??.po; do
 	langcode=$(basename $lang .po)
 	mkdir -p $langcode
-	asciidoctor     -a imagesdir=../ -o $langcode/index.$langcode.html -a lang=$langcode index.$langcode.adoc
-	asciidoctor-pdf -a media=print   -o $langcode/index.$langcode.pdf  -a lang=$langcode index.$langcode.adoc
+	asciidoctor     -o $langcode/index.$langcode.html -a lang=$langcode index.$langcode.adoc
+	asciidoctor-pdf -o $langcode/index.$langcode.pdf -a lang=$langcode index.$langcode.adoc
 
-	cat > index.$langcode.asis <<EOF
-Status: 303 See Other
-Location: ./$langcode/
-Content-Type: text/html; charset=UTF-8
-Content-Language: $langcode
-Vary: negotiate,accept-language
+	cat > index.$langcode.asis <<-EOF
+		Status: 303 See Other
+		Location: ./$langcode/
+		Content-Type: text/html; charset=UTF-8
+		Content-Language: $langcode
+		Vary: negotiate,accept-language
 
-See <a href="./$langcode/">$langcode</a>.
-EOF
+		See <a href="./$langcode/">$langcode</a>.
+	EOF
 
 done
 
